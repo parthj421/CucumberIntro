53cfc7d48bb6acec2d3dbb0b079f767d009dad8e
VIX\pjadeja <parthraj.jadeja@gmail.com>
2018-08-23 10:38:11 +0800

    Created Modified TestSuite

---
+++ Whisper.AutomationSuite/src/main/java/BaseUtil/BaseUtil.java
@@ -0,0 +1,16 @@
+package BaseUtil;
+
+import org.json.JSONObject;
+
+public class BaseUtil {
+	
+	public String PEMlog;
+	public JSONObject emvCard;
+	public String requestType;
+	public String cardScheme;
+	public String cardNumber;
+	public String txnAmount;	
+	public String partialURL;
+	
+
+}
